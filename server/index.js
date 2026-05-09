d6b80cac2f1a901eb5da588722efade319b2850c
prateek14x <prateek14x@gmail.com>
2026-05-09 20:31:46 +0530

    Configure Railway MongoDB connection and deployment settings

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001","https://teamtaskify.netlify.app"],
+    origin: ["http://localhost:3000", "http://localhost:3001","https://teamtaskify.netlify.app", "https://taskflow-mern-production-b7bb.up.railway.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
